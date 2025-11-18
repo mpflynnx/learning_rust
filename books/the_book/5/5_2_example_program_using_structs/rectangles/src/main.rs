be1a63aada744e2016af4dba103fe8c4c0311f34
mpflynnx <mpflynnx@gmail.com>
2025-11-18 11:58:13 +0000

    Added third iteration: - Function area() now takes Rectangle Struct - Better, struct conveys width and height are related - Easier to understand the meaning of related parameters - Could be improved, Struct fields are copied to area(), main() cannot use the rect1 struct again.

--- books/the_book/5/5_2_example_program_using_structs/rectangles/src/main.rs
+++ books/the_book/5/5_2_example_program_using_structs/rectangles/src/main.rs
@@ -1,12 +1,25 @@
-fn area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn area(rectangle: Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
 
 fn main() {
-    let rect1 = (30, 50);
+    // create a 
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
         area(rect1)
     );
+
+    // cannot use rect1 again as moved into area()
+    // and destroyed when area completes 
+    // println!("{:?}", rect1);
 }
